eeb5fefb0ba667a900f4b1517abd0bfa63b5d7e4
touheedkazmi <touheedkazmi99@gmail.com>
2026-08-10 15:45:35 +0530

    Add QA approval message

--- force-app/main/default/lwc/projectReleaseStatus/projectReleaseStatus.js
+++ force-app/main/default/lwc/projectReleaseStatus/projectReleaseStatus.js
@@ -3,6 +3,6 @@ import { LightningElement } from 'lwc';
 export default class ProjectReleaseStatus extends LightningElement {
 
     get releaseMessage() {
-        return 'Project is ready for Production';
+        return 'Release approved by QA';
     }
 }
